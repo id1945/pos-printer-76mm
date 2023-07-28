b7d8563290cf7ecae619e9aee6e25f0a35c96c89
daidh <duonghuudai.id002@gmail.com>
2023-07-28 22:00:14 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# PosPrinter76
+
+###### Android: https://play.google.com/store/apps/details?id=com.xinye.barcode4&hl=en_US
+###### PC: https://www.seagullscientific.com/support/downloads/drivers/4barcode/
+
+https://github.com/id1945/Pos-Printer-4barcode-Template/assets/40824445/c8e9b4a0-cbb2-405e-8725-eb20f5b5b3a5
+
+#### Paper size 76x125
+
+![](https://raw.githubusercontent.com/id1945/Pos-Printer-4barcode-Template/master/src/assets/02.jpg)
+![](https://raw.githubusercontent.com/id1945/Pos-Printer-4barcode-Template/master/src/assets/03.jpg)
+
+#### Model N80B
+
+![](https://raw.githubusercontent.com/id1945/Pos-Printer-4barcode-Template/master/src/assets/04.webp)
+![](https://raw.githubusercontent.com/id1945/Pos-Printer-4barcode-Template/master/src/assets/05.webp)
+![](https://raw.githubusercontent.com/id1945/Pos-Printer-4barcode-Template/master/src/assets/06.webp)
+![](https://raw.githubusercontent.com/id1945/Pos-Printer-4barcode-Template/master/src/assets/07.webp)
+![](https://raw.githubusercontent.com/id1945/Pos-Printer-4barcode-Template/master/src/assets/08.webp)
---
+++ src/app/app.component.ts
@@ -0,0 +1,100 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { NgxPrintElementService } from 'ngx-print-element';
+
+interface Pos {
+  name: string;
+  phone: string;
+  address: string;
+  content: string;
+  code: string;
+  sum: number;
+  description: string;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  
+  public formGroup: FormGroup;
+  public setting = {
+    style: {
+      fontSize: '19'
+    },
+    shop: {
+      name: 'Z.BRASS',
+      address: 'Địa chỉ: Di Trạch, Hoài Đức, Hà Nội',
+      phone: 'Hotline: 0845.882.882',
+      logo: {
+        width: '100',
+        height: '100',
+        url: 'assets/01.jpg'
+      }
+    },
+    sending: {
+      name: 'Tên người nhận:',
+      address: 'Địa chỉ:',
+      phone: 'Điện thoại:',
+      content: 'Nội dung:',
+      code: 'Mã vận đơn:',
+      sum: 'Tổng thu COD:',
+      description: 'Ghi chú:',
+      currency: 'VNĐ'
+    }
+  }
+
+  constructor(public print: NgxPrintElementService) {
+    const store = localStorage.getItem('Z.BRASS');
+    const posData: Pos = store ? JSON.parse(store) : null;
+    this.formGroup = new FormGroup({
+      name: new FormControl(posData?.name, {
+        validators: [Validators.required],
+      }),
+      phone: new FormControl(posData?.phone, {
+        validators: [Validators.required],
+      }),
+      address: new FormControl(posData?.address, {
+        validators: [Validators.required],
+      }),
+      content: new FormControl(posData?.content, {
+        validators: [Validators.required],
+      }),
+      code: new FormControl(posData?.code),
+      sum: new FormControl(posData?.sum ?? 0, { 
+        nonNullable: true,
+        validators: [Validators.required],
+      }),
+      description: new FormControl(posData?.description),
+    });
+
+    const settingDefault = localStorage.getItem('Z.BRASS.SETTING');
+    if (settingDefault) {
+      this.setting = JSON.parse(settingDefault);
+    }
+  }
+
+  get f() { return this.formGroup.controls; }
+
+  onClick() {
+    this.formGroup.markAllAsTouched();
+    if (this.formGroup.valid) {
+      const formData: Pos = this.formGroup.value;
+      this.print.print('demo').subscribe(console.log);
+      localStorage.setItem('Z.BRASS', JSON.stringify(formData));
+    }
+  }
+
+  onSettingSave() {
+    localStorage.setItem('Z.BRASS.SETTING', JSON.stringify(this.setting));
+  }
+
+  onReset() {
+    this.formGroup.reset();
+    localStorage.removeItem('Z.BRASS');
+    localStorage.removeItem('Z.BRASS.SETTING');
+    document.location.reload();
+  }
+}
