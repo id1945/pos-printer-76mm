ae8d87374531686775e4c893b5c664e925119213
DaiDH <40824445+id1945@users.noreply.github.com>
2023-12-06 22:23:57 +0700

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,7 +31,7 @@ export class AppComponent {
       title: 'Z.BRASS',
       name: 'Người gửi: Dương Hữu Đại',
       address: 'Địa chỉ: Hồ Tùng Mậu, Cầu Giấy, Hà Nội',
-      phone: 'Điện thoại: 0845.882.882',
+      phone: 'Số điện thoại: 0845.882.882',
       logo: {
         width: '100',
         height: '100',
