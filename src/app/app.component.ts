7c5ef09f0e3e4fd7b6e7ec9f32094e9bdbb74391
daidh <duonghuudai.id002@gmail.com>
2023-12-17 13:04:39 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { NgxPrintElementService } from 'ngx-print-element';
 
 interface Pos {
+  id?: string;
   name: string;
   phone: string;
   address: string;
@@ -19,7 +20,7 @@ interface Pos {
 })
 export class AppComponent {
   
-  public formGroup: FormGroup;
+  public formGroup!: FormGroup;
   public setting = {
     style: {
       fontSize: '19',
@@ -50,10 +51,23 @@ export class AppComponent {
       format: '1.0' // https://stackblitz.com/edit/angular-currency-decimal-percent-pipe
     }
   }
+  public data: Pos[] = [];
 
   constructor(public print: NgxPrintElementService) {
     const store = localStorage.getItem('Z.BRASS');
     const posData: Pos = store ? JSON.parse(store) : null;
+    this.initForm(posData);
+
+    const settingDefault = localStorage.getItem('Z.BRASS.SETTING');
+    if (settingDefault) {
+      this.setting = JSON.parse(settingDefault);
+    }
+
+    const storeData = localStorage.getItem('Z.BRASS.DATA');
+    this.data = storeData ? JSON.parse(storeData) : [];
+  }
+
+  initForm(posData: Pos) {
     this.formGroup = new FormGroup({
       name: new FormControl(posData?.name, {
         validators: [Validators.required],
@@ -74,11 +88,6 @@ export class AppComponent {
       }),
       description: new FormControl(posData?.description),
     });
-
-    const settingDefault = localStorage.getItem('Z.BRASS.SETTING');
-    if (settingDefault) {
-      this.setting = JSON.parse(settingDefault);
-    }
   }
 
   get f() { return this.formGroup.controls; }
@@ -88,7 +97,15 @@ export class AppComponent {
     if (this.formGroup.valid) {
       const formData: Pos = this.formGroup.value;
       this.print.print('demo').subscribe(console.log);
+      // Add to list
+      const store = localStorage.getItem('Z.BRASS.DATA');
+      if (store) {
+        this.data.push({ id: Math.random() * 1000 + '', ...formData });
+        this.data = [...JSON.parse(store), ...this.data];
+      }
+      // Save db
       localStorage.setItem('Z.BRASS', JSON.stringify(formData));
+      localStorage.setItem('Z.BRASS.DATA', JSON.stringify(this.data));
     }
   }
 
@@ -101,5 +118,17 @@ export class AppComponent {
     localStorage.removeItem('Z.BRASS');
     localStorage.removeItem('Z.BRASS.SETTING');
     document.location.reload();
+  }
+
+  onSelected(item: Pos) {
+    this.initForm(item);
+  }
+  
+  onDelete(item: Pos) {
+    const remove = this.data.filter(f => f.id !== item.id);
+    if (remove.length) {
+      this.data = remove;
+      localStorage.setItem('Z.BRASS.DATA', JSON.stringify(remove));
+    }
   }
 }
